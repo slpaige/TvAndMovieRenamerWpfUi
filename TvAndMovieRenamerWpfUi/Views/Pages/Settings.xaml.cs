db495da1ebf1bf40ae8c442d346752edbe9b4890
Shannon Paige <shannon.paige@outlook.com>
2022-07-02 11:17:48 -0500

    Add project files.

---
+++ TvAndMovieRenamerWpfUi/Views/Pages/Settings.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TvAndMovieRenamerWpfUi.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : Page
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TvAndMovieRenamerWpfUi/Views/Pages/TvShows.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TvAndMovieRenamerWpfUi.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class TvShows : Page
+    {
+        public TvShows()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //var parent = Window.GetWindow(this);
+            var parent = App.Current.MainWindow;
+            
+            var modal = new Modal();
+            modal.Top = parent.Top + 100;
+            modal.Left = parent.Left + 100;
+            //modal.Show();
+            modal.ShowDialog();
+        }
+    }
+}
